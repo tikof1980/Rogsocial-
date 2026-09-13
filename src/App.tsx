c21bf81851c77a0b7c06271e6977dd085ddf932d
tikof1980 <serykouame@gmail.com>
2026-09-13 20:28:59 +0000

    Fix import statement formatting in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import  React from 'react';
 import { useApp } from './context/AppContext';
 import TopBar from './components/layout/TopBar';
 import BottomNav from './components/layout/BottomNav';
