8fe927f5d445e62642524899b08ea327b43eec66
tikof1980 <serykouame@gmail.com>
2026-09-19 08:18:20 +0000

    Replace AppProvider with AuthProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AppProvider } from './context/AppContext';
+import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppProvider>
+    <AuthProvider>
       <App />
-    </AppProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
